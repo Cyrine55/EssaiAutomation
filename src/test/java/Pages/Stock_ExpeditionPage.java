adabd00b36c040fd2f9d1b74b524dcfde25fedce
yefcyrine <yefrni.syrine@gmail.com>
2024-05-29 16:30:35 +0100

    POM pour expedition

--- src/test/java/Pages/Stock_ExpeditionPage.java
+++ src/test/java/Pages/Stock_ExpeditionPage.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,32 +14,51 @@ import Formation.Utils.BaseUtils;
 
 public class Stock_ExpeditionPage extends BaseUtils {
 	
+	public Stock_ExpeditionPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//span[contains(text(),'Gestion de stock')]")
+	WebElement GestionStockBtn;
+	
+	@FindBy(xpath="//div[2]/ul/li[1]/span")
+	WebElement ExpeditionBtn;
+	
+	@FindBy(xpath="html/body/app-root/app-layout/nz-layout/nz-layout/nz-layout/nz-content/app-list-shipments/header/div[2]/nz-range-picker/div[1]/input")
+	WebElement Datedebut;
+	
+	@FindBy(xpath="//nz-range-picker/div[1]/input")
+	WebElement debut;
+	@FindBy(xpath="//input[@placeholder='Date de fin']")
+	WebElement DateFin;
+	
+	@FindBy(xpath="//nz-range-picker/div[3]/input")
+	WebElement fin;
+	
+	String dateDebut = "21/05/2024";
+    String dateFin="01/06/2024";
+	
 	
 	public void ecran_expéditions_affiche() {
-	 WebElement GestionStockBtn= driver.findElement(By.xpath("//span[contains(text(),'Gestion de stock')]"));
 	 GestionStockBtn.click();
-	 WebElement ExpeditionBtn=driver.findElement(By.xpath("//div[2]/ul/li[1]/span"));
 	 ExpeditionBtn.click(); 
 	  }
 	
 	public void choisir_un_interval_de_date_depuis_calendrier() throws InterruptedException {
 	   	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-	      WebElement Datedebut=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("html/body/app-root/app-layout/nz-layout/nz-layout/nz-layout/nz-content/app-list-shipments/header/div[2]/nz-range-picker/div[1]/input")));
-	      Datedebut.click();
-	      WebElement debut=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//nz-range-picker/div[1]/input")));
-	   
+	    wait.until(ExpectedConditions.elementToBeClickable(Datedebut));
+	    Datedebut.click();
+	   wait.until(ExpectedConditions.visibilityOf(debut));
 	      JavascriptExecutor js = (JavascriptExecutor) driver;
-	      String dateDebut = "30/05/2024";
-	       String dateFin="01/06/2024";
+	      
 	  
 	       js.executeScript("arguments[0].value=arguments[1];", debut, dateDebut);
 	       js.executeScript("arguments[0].dispatchEvent(new Event('change'));", debut);
 	       Thread.sleep(3000);
 	       js.executeScript("document.querySelector('body').click();");//pour fermer le 1er calendrier
-	       Thread.sleep(3000);
-	       WebElement DateFin = driver.findElement(By.xpath("//input[@placeholder='Date de fin']"));
+	       Thread.sleep(2000);
 	       DateFin.click();
-	       WebElement fin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//nz-range-picker/div[3]/input")));
+	       wait.until(ExpectedConditions.visibilityOf(DateFin));
 		   js.executeScript("arguments[0].value=arguments[1];", fin, dateFin);
 		   js.executeScript("arguments[0].dispatchEvent(new Event('change'));", fin);
 		    Thread.sleep(1000); 
--- src/test/java/Pages/SupprimerSitePage.java
+++ src/test/java/Pages/SupprimerSitePage.java
@@ -10,6 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,6 +19,20 @@ import Formation.Utils.BaseUtils;
 
 public class SupprimerSitePage extends BaseUtils {
 	
+	public SupprimerSitePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//tbody/tr[6]/td[7]/em[2]")
+	 WebElement Btn_Supprimer;
+	
+	@FindBy(xpath="//div[contains(text(),'Êtes-vous sûr de bien vouloir supprimer ce site?')]")
+	 WebElement MsgElement;
+	
+	@FindBy(xpath="//div[@class='content-text']")
+	 WebElement container;
+	@FindBy(xpath="//span[normalize-space()='Oui']")
+	 WebElement Btn_oui;
 	/*public void selectionner_un_site(int index) {
 	    
 		List<WebElement> CheckSiteList=driver.findElements(By.xpath("//input[@type='checkbox']"));
@@ -43,13 +59,11 @@ public class SupprimerSitePage extends BaseUtils {
 	}*/
 	
 	public void cliquer_sur_le_bouton_supprimer() {
-	    WebElement Btn_Supprimer=driver.findElement(By.xpath("//tbody/tr[6]/td[7]/em[2]"));
 	    Btn_Supprimer.click();
 	}
 	
 	public void le_message_de_suppression_est_affiche(String message) {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		WebElement MsgElement= driver.findElement(By.xpath("//div[contains(text(),'Êtes-vous sûr de bien vouloir supprimer ce site?')]"));
 		String Msg_suppression=MsgElement.getText().trim();
 		System.out.println("Message de suppression souhaité : " + message);
 		System.out.println("Message de suppression obtenue : " + Msg_suppression);
@@ -59,16 +73,12 @@ public class SupprimerSitePage extends BaseUtils {
 
 	
 	public void cliquer_sur_oui() {
-		WebElement container= driver.findElement(By.xpath("//div[@class='content-text']"));
-		WebElement Btn_oui=container.findElement(By.xpath("//span[normalize-space()='Oui']"));
 	   Btn_oui.click();
 	}
 
 	
 	public void site_supprime_avec_un_message_succes() throws InterruptedException {
 		Thread.sleep(2000);
-	    //WebElement Success_alert=driver.findElement(By.className("ant-notification-notice-message ng-tns-c53-87"));
-		
 		WebElement Success_alert=new WebDriverWait(driver,Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfElementLocated(By.className("ant-notification-notice-message")));
 	    assertTrue(Success_alert.isDisplayed());
 	}
